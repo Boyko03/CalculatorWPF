d6a1baf4fd22e0aa59c0a056269f09148f9e7ec1
Boyko03 <221426@buas.nl>
2025-01-11 21:39:34 +0100

    Split app into multiple controls to imrove readability

---
+++ Controls/ResultBar.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator.Controls
+{
+    /// <summary>
+    /// Interaction logic for ResultBar.xaml
+    /// </summary>
+    public partial class ResultBar : UserControl
+    {
+        public new static readonly DependencyProperty ForegroundProperty = DependencyProperty.Register(nameof(Foreground), typeof(Brush), typeof(ResultBar), new PropertyMetadata(Brushes.White));
+
+        public ResultBar()
+        {
+            InitializeComponent();
+        }
+
+        public new Brush Foreground
+        {
+            get => (Brush)GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+    }
+}
